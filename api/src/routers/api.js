9f421951d21955bf3c756d05c850fbf928bc31a5
hiwot <99hiwot@gmail.com>
2026-05-07 23:40:42 +0200

    feat: Updated

--- api/src/routers/api.js
+++ api/src/routers/api.js
@@ -1,8 +1,14 @@
 import express from "express";
 import eventsRouter from "#routers/events.js";
 
+import authRouter from "#routers/auth.js";
+import cartRouter from "#routers/cart.js";
+
 const apiRouter = express.Router();
 
 apiRouter.use("/events", eventsRouter);
+
+apiRouter.use("/auth", authRouter);
+apiRouter.use("/cart", cartRouter);
 
 export default apiRouter;

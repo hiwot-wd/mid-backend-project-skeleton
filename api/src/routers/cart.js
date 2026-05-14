cac62b13958998ecc51ce4807107ace9e2f14352
hiwot <99hiwot@gmail.com>
2026-05-14 21:42:47 +0200

    feat: Checkout end point added

--- api/src/routers/cart.js
+++ api/src/routers/cart.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { auth } from "#middlewares/auth.js";
-import { getCart, addItem, updateItem, removeItem } from "#controllers/cart.js";
+import {
+  getCart,
+  addItem,
+  updateItem,
+  removeItem,
+  checkout,
+} from "#controllers/cart.js";
 
 const router = express.Router();
 
@@ -11,5 +17,5 @@ router.get("/", getCart);
 router.post("/items", addItem);
 router.patch("/items/:itemId", updateItem);
 router.delete("/items/:itemId", removeItem);
-
+router.post("/checkout", checkout);
 export default router;
